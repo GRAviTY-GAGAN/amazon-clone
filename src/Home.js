66b768a40453a67b20e7c3c47259b9157fb69cb6
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-10-17 12:31:50 +0530

    Added context API

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <img
           className="home__image"
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-          alt="home image"
+          alt="home pic"
         />
 
         <div className="home__row">
@@ -17,14 +17,14 @@ const Home = () => {
             id={6565465655}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
             title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-            price={29.99}
+            price={613}
             rating={5}
           />
           <Product
             id={5451324556}
             image="https://m.media-amazon.com/images/I/614TSm1LckL._SX425_.jpg"
             title="PHILIPS Air Fryer HD9200/90, uses up to 90% less fat, 1400W, 4.1 Liter, with Rapid Air Technology"
-            price={85.99}
+            price={"6,599"}
             rating={5}
           />
         </div>
@@ -33,21 +33,21 @@ const Home = () => {
             id={5545887525}
             image="https://m.media-amazon.com/images/I/81FyYXoQF-L._SY500_.jpg"
             title="Samsung 192 L 2 Star Direct Cool Single Door Refrigerator"
-            price={258}
+            price={"12,790"}
             rating={5}
           />
           <Product
             id={84215651}
             image="https://m.media-amazon.com/images/I/71Kx6rgmlRS._UX342_.jpg"
             title="TIMEX Analog Men's Watch"
-            price={15}
+            price={898}
             rating={4}
           />
           <Product
             id={942215458}
             image="https://m.media-amazon.com/images/I/91DmobHvTpL._UY550_.jpg"
             title="Skybags Sonic 49 Ltrs Green Rucksack"
-            price={55}
+            price={"1,719"}
             rating={4}
           />
         </div>
@@ -56,7 +56,7 @@ const Home = () => {
             id={942215458}
             image="https://m.media-amazon.com/images/I/811CjK6UMFL._SX450_.jpg"
             title="Acer Ei491Cr 49 Inch (124.46 cm) Led 1800R Curved Dfhd 3840 X 1080 Pixels Va Monitor I 32:9 Aspect Ratio I AMD Radeon Freesync2 Technology I 144Hz I Vesa Certified Display HDR 400 I Dci-P3 Black"
-            price={550}
+            price={"79,999"}
             rating={3}
           />
         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,33 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 const Product = ({ id, title, image, price, rating }) => {
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log("THIS IS BASKET", basket);
+
+  const addToBasket = () => {
+    // dispatch item into to data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
-          <small>$</small>
-          <strong>{price}</strong>
+          <small>₹ </small>
+          <strong> {price}</strong>
         </p>
         <div className="product__rating">
           {/* using the rating prop to display no of stars dynamically */}
@@ -20,9 +39,9 @@ const Product = ({ id, title, image, price, rating }) => {
         </div>
       </div>
 
-      <img src={image} alt="product image" />
+      <img src={image} alt="product pic" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 };
